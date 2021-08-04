a4e09878311bc97539d9a4d1e2771a4cf1c86857
DSnoddy <douglassnoddyjr@gmail.com>
2021-08-04 12:03:19 -0800

    update to json package

--- src/components/About.js
+++ src/components/About.js
@@ -15,9 +15,10 @@ export default function About() {
                     </p>
                     <div className="flex justify-center">
                         <a
-                            href="#contact"
+                            href=""
+                            download=""
                             className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
-                            Downlaod Resume
+                            Download Resume
                         </a>
                         <a
                             href="#projects"
